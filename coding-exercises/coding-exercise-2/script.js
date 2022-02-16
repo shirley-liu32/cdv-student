7761dbc48e015e29c79dfa02f76bd3d19e27890a
Shirley Liu <sl6895@nyu.edu>
2022-02-17 05:55:40 +0800

    finished coding exercise 2

---
+++ coding-exercises/coding-exercise-2/script.js
@@ -0,0 +1,316 @@
+let data = [
+    {
+        "timestamp": "2022-02-16T09:40:57.201Z",
+        "Chopped Cheese": 10,
+        "Arizona Green Tea": 10,
+        "Takis": 10,
+        "Honey Bun": 10,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 5,
+        "Sour Power Straws": 10
+    },
+    {
+        "timestamp": "2022-02-16T09:41:57.329Z",
+        "Chopped Cheese": 8,
+        "Arizona Green Tea": 6,
+        "Takis": 9,
+        "Honey Bun": 3,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 2,
+        "Sour Power Straws": 9
+    },
+    {
+        "timestamp": "2022-02-16T09:44:41.616Z",
+        "Chopped Cheese": 3,
+        "Arizona Green Tea": 10,
+        "Takis": 7,
+        "Honey Bun": 6,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 4,
+        "Sour Power Straws": 8
+    },
+    {
+        "timestamp": "2022-02-16T09:47:11.370Z",
+        "Chopped Cheese": 7,
+        "Arizona Green Tea": 6,
+        "Takis": 6,
+        "Honey Bun": 3,
+        "Bacon Egg & Cheese": 8,
+        "Fruit Punch": 2,
+        "Sour Power Straws": 3
+    },
+    {
+        "timestamp": "2022-02-16T10:00:41.393Z",
+        "Chopped Cheese": 10,
+        "Arizona Green Tea": 6,
+        "Takis": 4,
+        "Honey Bun": 7,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 8,
+        "Sour Power Straws": 4
+    },
+    {
+        "timestamp": "2022-02-16T10:16:44.664Z",
+        "Chopped Cheese": 10,
+        "Arizona Green Tea": 10,
+        "Takis": 5,
+        "Honey Bun": 5,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 5,
+        "Sour Power Straws": 8
+    },
+    {
+        "timestamp": "2022-02-16T12:33:09.434Z",
+        "Chopped Cheese": 7,
+        "Arizona Green Tea": 10,
+        "Takis": 8,
+        "Honey Bun": 5,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 2,
+        "Sour Power Straws": 7
+    },
+    {
+        "timestamp": "2022-02-16T13:05:28.492Z",
+        "Chopped Cheese": 8,
+        "Arizona Green Tea": 8,
+        "Takis": 8,
+        "Honey Bun": 7,
+        "Bacon Egg & Cheese": 9,
+        "Fruit Punch": 7,
+        "Sour Power Straws": 6
+    },
+    {
+        "timestamp": "2022-02-16T13:23:14.030Z",
+        "Chopped Cheese": 10,
+        "Arizona Green Tea": 10,
+        "Takis": 8,
+        "Honey Bun": 7,
+        "Bacon Egg & Cheese": 8,
+        "Fruit Punch": 3,
+        "Sour Power Straws": 8
+    },
+    {
+        "timestamp": "2022-02-16T13:25:46.945Z",
+        "Chopped Cheese": 8,
+        "Arizona Green Tea": 10,
+        "Takis": 10,
+        "Honey Bun": 5,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 6,
+        "Sour Power Straws": 10
+    },
+    {
+        "timestamp": "2022-02-16T13:31:14.937Z",
+        "Chopped Cheese": 7,
+        "Arizona Green Tea": 10,
+        "Takis": 9,
+        "Honey Bun": 7,
+        "Bacon Egg & Cheese": 9,
+        "Fruit Punch": 6,
+        "Sour Power Straws": 10
+    },
+    {
+        "timestamp": "2022-02-16T13:33:01.162Z",
+        "Chopped Cheese": 2,
+        "Arizona Green Tea": 9,
+        "Takis": 1,
+        "Honey Bun": 10,
+        "Bacon Egg & Cheese": 1,
+        "Fruit Punch": 9,
+        "Sour Power Straws": 9
+    },
+    {
+        "timestamp": "2022-02-16T13:39:17.748Z",
+        "Chopped Cheese": 10,
+        "Arizona Green Tea": 10,
+        "Takis": 10,
+        "Honey Bun": 7,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 2,
+        "Sour Power Straws": 10
+    },
+    {
+        "timestamp": "2022-02-16T14:14:12.831Z",
+        "Chopped Cheese": 9,
+        "Arizona Green Tea": 2,
+        "Takis": 6,
+        "Honey Bun": 8,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 5,
+        "Sour Power Straws": 10
+    },
+    {
+        "timestamp": "2022-02-16T14:21:24.697Z",
+        "Chopped Cheese": 8,
+        "Arizona Green Tea": 10,
+        "Takis": 8,
+        "Honey Bun": 10,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 6,
+        "Sour Power Straws": 9
+    },
+    {
+        "timestamp": "2022-02-16T14:27:21.383Z",
+        "Chopped Cheese": 5,
+        "Arizona Green Tea": 10,
+        "Takis": 7,
+        "Honey Bun": 2,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 4,
+        "Sour Power Straws": 4
+    },
+    {
+        "timestamp": "2022-02-16T14:36:21.847Z",
+        "Chopped Cheese": 9,
+        "Arizona Green Tea": 1,
+        "Takis": 8,
+        "Honey Bun": 4,
+        "Bacon Egg & Cheese": 9,
+        "Fruit Punch": 3,
+        "Sour Power Straws": 5
+    },
+    {
+        "timestamp": "2022-02-16T14:42:55.077Z",
+        "Chopped Cheese": 8,
+        "Arizona Green Tea": 10,
+        "Takis": 10,
+        "Honey Bun": 10,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 5,
+        "Sour Power Straws": 8
+    },
+    {
+        "timestamp": "2022-02-16T14:46:42.126Z",
+        "Chopped Cheese": 8,
+        "Arizona Green Tea": 10,
+        "Takis": 2,
+        "Honey Bun": 6,
+        "Bacon Egg & Cheese": 10,
+        "Fruit Punch": 5,
+        "Sour Power Straws": 10
+    },
+    {
+        "timestamp": "2022-02-16T15:05:22.099Z",
+        "Chopped Cheese": 8,
+        "Arizona Green Tea": 7,
+        "Takis": 5,
+        "Honey Bun": 5,
+        "Bacon Egg & Cheese": 8,
+        "Fruit Punch": 6,
+        "Sour Power Straws": 4
+    }
+]
+
+
+
+
+
+// A function Leon wrote to transform the structure of the data
+// The function with many comments on description of what kind of
+// data it transform into what other kind of data is here:
+// https://github.com/leoneckert/critical-data-and-visualization-spring-2022/tree/master/labs/lab1#transform-data
+
+
+function averageData(data){
+    let newData = [];
+    let keys = Object.keys(data[ data.length-1 ]);
+    for(let i = 0; i < keys.length; i++){
+      let key = keys[i];
+      let sum = 0;
+      let num = 0;
+      for(let j = 0; j < data.length; j++){
+        let datum = data[j];
+        if(key in datum){
+          sum += datum[key];
+          num++;
+        }
+      }
+      let avg = sum/num;
+      if(!isNaN(avg)){
+        let newDataPoint = {"name": key, "average": avg, 'numMeasurements': num};
+        newData.push(newDataPoint);
+      }
+    }
+    return newData;
+  }
+  
+  // here we use the function to transform the data
+  let transformedData = averageData(data);
+  
+  
+  // log things to the console as you go to make sure things
+  // are behaving as intended. I see the data looks good
+  console.log(transformedData);
+  
+  // up here, we also select the container (div) on the page
+  // into which we insert the bars we create in the loop below
+  let container = document.getElementById("container");
+  
+  
+  // Loop over the data, once for each datapoint.
+  for(let i = 0; i < transformedData.length; i++){
+    // get the item we deal with at this iteration
+    let datapoint = transformedData[i];
+    // datapoint looks like this in the first iteration:
+    // {
+    //   average: 3.2857142857142856
+    //   name: "verySaltyFish"
+    //   numMeasurements: 7
+    // }
+  
+    // get the name of the current
+    //  item by "digging" into the datapoint object
+    // and the average liking value
+    let food = datapoint.name;
+    let average = datapoint.average;
+  
+    // in each iteration (once for each food)
+    // we create a div
+    let bar = document.createElement("div");
+  
+    // next, we assign the className to the div
+    // that will make sure basic bar styling (defined in css/style.css)
+    // is applied to it (like height, color, margin)
+    bar.className = "bar";
+    // because the width is different for each bar
+    // we define it right here in javascript using the
+    // average value of each data point that we iterate over
+    // we stored average value in the variable "average" on line 178
+
+    //set width and height of bar - change to circle with border radius
+    bar.style.width = (average * 26) + "px";
+    bar.style.height = (average * 26) + "px";
+
+  
+    // labels
+    // so far we have a div that has a width that corresponds
+    // to the data value. next we create a p-tag the
+    // text (innerHTML) of which is the name of the food currently
+    // iterated over
+    // we stored the name in the variable "food" on line 184
+
+    // create elements that will store name and average
+    let barname = document.createElement("h4");
+    let barAvg = document.createElement("p");
+    // change the text
+    barname.innerHTML = food;
+    barAvg.innerHTML = average;
+    // now here we insert the p tag into the div tag we created above
+    
+    //adds name and average of the snack rating
+    bar.appendChild(barname);
+    bar.appendChild(barAvg);
+    // at this point the element we have created using JavaScript only
+    // looks like:
+    // <div class="bar"><p>watermelonWithFetaCheese</p></div>
+    // (that is for the first datapoint (watermelonWithFetaCheese) that we iterate over)
+  
+    // that whole element (the div containing the p tag)
+    // we append to the body after all
+    // bring it from "JavaScript world" to "HTML world"
+    container.appendChild(bar);
+  
+  
+  }
+
+   
